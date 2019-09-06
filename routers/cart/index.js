78d09d0df0931dc917ef7e1287c60f211918de00
Nguyen Thanh Duy <nguyenthanhduy@Nguyens-MacBook-Pro.local>
2019-09-06 23:27:06 +0700

    update api cart and booking

--- routers/cart/index.js
+++ routers/cart/index.js
@@ -45,4 +45,18 @@ router.post('/updateCart', auth , async (req,res) => {
    
 });
 
+router.get('/getCartByIdUser',auth,async (req,res) => {
+    try {
+        const cart = await carts.findById(req.id);
+
+        res.json({cart});
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(501).json({msg : 'server error'});
+    }
+   
+})
+
 module.exports = router;
