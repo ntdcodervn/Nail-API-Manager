30266d4974dcb72b5c8615066389668d5fc25fea
Nguyen Thanh Duy <nguyenthanhduy@Nguyens-MacBook-Pro.local>
2019-09-10 00:06:17 +0700

    update booked api

--- routers/booking/booking.js
+++ routers/booking/booking.js
@@ -83,7 +83,7 @@ router.get('/getAllBook',auth,[
 
 router.get('/getBooked',auth, async (req,res) => {
     try {
-        let booked = booking.find({users : req.id});
+        let booked = await booking.find({users : req.id});
         res.json({booked});
     } catch (error) {
         res.status(501).json({msg : 'server error'})
